c7e06c45b5772d2c6b9420d1358c04c66242358a
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-08 08:38:41 +0600

    map and slice complete

--- 1-Overview-of-Go-Language/6-map-and-slices/main.go
+++ 1-Overview-of-Go-Language/6-map-and-slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 type User struct {
 	FirstName string
@@ -21,5 +24,23 @@ func main() {
 	}
 	myName["name"] = me
 	log.Println(myName["name"])
+
+	// slice area start
+
+	var mySlice []string
+
+	mySlice = append(mySlice, "Riyaz", "Khan", "Shuvo")
+	mySlice = append(mySlice, "Riyaz", "Khan", "Shuvo")
+
+	log.Println(mySlice)
+
+	//  to sort the slice element
+
+	mySliceElement := []int{1, 5, 3, 7, 15, 9, 11, 13}
+
+	log.Println("The Slice element is : ", mySliceElement)
+	sort.Ints(mySliceElement)
+	log.Println("The Slice element after sort: ", mySliceElement)
+	log.Println(mySliceElement[5:7]) //this is the range
 
 }
