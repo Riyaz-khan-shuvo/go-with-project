7f15fae3d0873dc38045bcc50a59199aadfaf326
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-09 22:40:42 +0600

    reading json is complete

---
+++ 1-Overview-of-Go-Language/13-reading-writing-json/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	firstName string `json:"first_name,string"`
+	lastName  string `json:"last_name"`
+	hairColor string `json:"hair_color"`
+	hasDog    bool   `json:"has_dog"`
+}
+
+func main() {
+
+	myJson := `[
+		{
+			"first_name":"Riyaz",
+			"last_name":"Hossain",
+			"hair_color":"Black",
+			"has_dog":true
+		},
+		{
+			"first_name":"Shahid",
+			"last_name":"Khan",
+			"hair_color":"Black",
+			"has_dog":false
+		}
+	]`
+
+	var u []Person
+
+	err := json.Unmarshal([]byte(myJson), &u)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("unmarshal : %v", u)
+
+}
