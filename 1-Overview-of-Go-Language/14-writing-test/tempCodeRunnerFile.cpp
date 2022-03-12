89ca59d6ff90f9949fb9d22be285a5dea5a6d502
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-12 23:34:07 +0600

    clean coading complete

---
+++ 1-Overview-of-Go-Language/14-writing-test/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if (a > b && a > c)
+    // {
+    //     result = ((a + b) + abs(a - b)) / 2;
+    // }
--- 2-Building-a-Basic-Web-Application/9-working-with-layout/pkg/render/render.go
+++ 2-Building-a-Basic-Web-Application/9-working-with-layout/pkg/render/render.go
@@ -4,12 +4,57 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, temp string) {
+
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("error getting template cache : ")
+
+	}
 	pToTem, _ := template.ParseFiles("../../templates/" + temp)
-	err := pToTem.Execute(w, nil)
+	err = pToTem.Execute(w, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("../../templates/*.gohtml")
+	if err != nil {
+		return myCache, err
+	}
+	fmt.Println("pages : ", pages)
+
+	for _, page := range pages {
+
+		name := filepath.Base(page)
+
+		// ts = template set
+		fmt.Println("page is currently :", name)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("../../templates/*.layouthtml")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("../../templates/*.layouthtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
