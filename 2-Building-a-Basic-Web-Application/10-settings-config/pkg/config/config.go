65377b4ff639454dac943e24484f272ca43e538a
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-14 12:39:03 +0600

    class-10 complete

--- 2-Building-a-Basic-Web-Application/10-settings-config/pkg/config/config.go
+++ 2-Building-a-Basic-Web-Application/10-settings-config/pkg/config/config.go
@@ -2,13 +2,10 @@ package config
 
 import (
 	"html/template"
-	"log"
 )
 
 // app config holds the application config.
 
 type AppConfig struct {
-	useCache      bool
 	TemplateCache map[string]*template.Template
-	infoLog       *log.Logger
 }
