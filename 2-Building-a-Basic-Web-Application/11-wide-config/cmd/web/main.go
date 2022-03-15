41031721e937ad5519c220aba4fb7615cd862051
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-15 21:10:33 +0600

    config file complete

--- 2-Building-a-Basic-Web-Application/11-wide-config/cmd/web/main.go
+++ 2-Building-a-Basic-Web-Application/11-wide-config/cmd/web/main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Riyaz-khan-shuvo/go-with-project/2-Building-a-Basic-Web-Application/11-wide-config/pkg/config"
 	"github.com/Riyaz-khan-shuvo/go-with-project/2-Building-a-Basic-Web-Application/11-wide-config/pkg/handlers"
+	"github.com/Riyaz-khan-shuvo/go-with-project/2-Building-a-Basic-Web-Application/11-wide-config/pkg/render"
 )
 
 const portNumber string = ":8000"
 
 func main() {
 
+	var app config.AppConfig
+	tc, err := render.CreateRenderPage()
+	if err != nil {
+		fmt.Println(err)
+	}
+	app.TemplateCache = tc
 	fmt.Println("Listing the server http://localhost" + portNumber)
 	http.HandleFunc("/", handlers.HomePage)
 	http.HandleFunc("/about", handlers.AboutPage)
--- 2-Building-a-Basic-Web-Application/11-wide-config/pkg/render/render.go
+++ 2-Building-a-Basic-Web-Application/11-wide-config/pkg/render/render.go
@@ -1,16 +1,72 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTem(w http.ResponseWriter, tem string) {
-	pToTem, err := template.ParseFiles("../../template/" + tem)
+
+	tc, err := CreateRenderPage()
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	pToTem.Execute(w, nil)
+
+	t, ok := tc[tem]
+	if !ok {
+		fmt.Println(ok)
+	}
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+	_, err = buf.WriteTo(w)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// pToTem, err := template.ParseFiles("../../template/" + tem)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// pToTem.Execute(w, nil)
+	// CreateRenderPage()
+}
+
+func CreateRenderPage() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("../../template/*.gohtml")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			fmt.Println(err)
+		}
+		matches, err := filepath.Glob("../../template/*.layouthtml")
+		if err != nil {
+			fmt.Println(err)
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("../../template/*layouthtml")
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
