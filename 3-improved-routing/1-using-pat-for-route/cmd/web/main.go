dc4f5844dc81329a275d78939a066b4c0ab11da8
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-22 19:02:23 +0600

    basic main function is ready

--- 3-improved-routing/1-using-pat-for-route/cmd/web/main.go
+++ 3-improved-routing/1-using-pat-for-route/cmd/web/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber string = ":8888"
+
+func main() {
+
+	fmt.Println("listing the port http://localhost" + portNumber)
+
+	http.ListenAndServe(portNumber, nil)
+}
